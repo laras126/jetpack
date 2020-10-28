61b005b17ae4df099e55e09943703287b5e5d0bb
Konstantin Obenland <obenland@gmx.de>
2020-10-28 14:45:48 -0700

    Move Jetpack menu on Atomic sites

--- modules/masterbar.php
+++ modules/masterbar.php
@@ -5,7 +5,7 @@
  * Sort Order: 38
  * Recommendation Order: 16
  * First Introduced: 4.8
- * Requires Connection: Yes
+ * Requires Connection: No
  * Auto Activate: No
  * Module Tags: General
  * Additional Search Queries: adminbar, masterbar, colorschemes
@@ -23,7 +23,7 @@ require dirname( __FILE__ ) . '/masterbar/admin-color-schemes/admin-color-scheme
 new Masterbar();
 new Admin_Color_Schemes();
 
-if ( apply_filters( 'jetpack_load_admin_menu_class', false ) ) {
+if ( apply_filters( 'jetpack_load_admin_menu_class', ( defined( 'IS_WPCOM' ) && IS_WPCOM ) || jetpack_is_atomic_site() ) ) {
 	require_once __DIR__ . '/masterbar/class-admin-menu.php';
 	Admin_Menu::get_instance();
 }
